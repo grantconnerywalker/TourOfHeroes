8eec9a9401d028b15a1e7dc7f1d2b7ab35300665
grantconnerywalker <grant.connery.walker@gmail.com>
2020-03-20 15:27:25 -0700

    Part 3 of tour complete

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+// cd D:\Users\Pills\AngularPractice\angular-tour-of-heroes 
+// ng serve --open 
+
+import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
